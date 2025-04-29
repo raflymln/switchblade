de01b1dc09b2ef3648ef5568757ef5b6dc588d20
Rafly Maulana <mraflymaulana@gmail.com>
2025-04-29 15:48:33 +0700

    build(package): try fix the exports to allow import for adapter

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
     entry: ["src/index.ts", "src/adapters/hono.ts"],
-    format: ["esm"],
+    format: ["esm", "cjs"],
     dts: true,
     platform: "node",
     target: "es2024",
